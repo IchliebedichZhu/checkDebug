4998808ac55b79ef9b86a648883cbc724e097268
IchliebedichZhu <IchliebedichZhu@gmail.com>
2021-08-24 10:45:26 +0800

    report error to server

--- package/ReactWatch/index.tsx
+++ package/ReactWatch/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Logger from '../API';
 
 export default class HandleError extends React.Component {
+  private logger: Logger;
+
+  constructor(props: Record<string, any>) {
+    super(props);
+    this.logger = new Logger(props.loggerKey, props.loggerBaseUrl);
+  }
+
   componentDidCatch(err: Error) {
     console.log('err', err);
+    this.logger.add({
+      type: 'error',
+      event: err.name,
+      source: err.stack || '',
+      line: 0,
+      column: 0,
+      error: err.message,
+    });
   }
 
   render() {
