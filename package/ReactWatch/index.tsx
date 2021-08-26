7f1b2c0567e68fd84455153dec3b7d4e5fd68171
IchliebedichZhu <IchliebedichZhu@gmail.com>
2021-08-26 09:42:22 +0800

    update handle React Error function

--- package/ReactWatch/index.tsx
+++ package/ReactWatch/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Logger from '../API';
 
-export default class HandleError extends React.Component {
+export type handleCatchError = (err: Error) => void;
+export type HandleErrorProps = {
+  handleError?: handleCatchError;
+  loggerKey: string;
+  loggerBaseUrl: string;
+};
+
+export default class HandleError extends React.Component<HandleErrorProps> {
   private logger: Logger;
 
-  constructor(props: Record<string, any>) {
+  constructor(props: HandleErrorProps) {
     super(props);
     this.logger = new Logger(props.loggerKey, props.loggerBaseUrl);
   }
@@ -19,6 +26,9 @@ export default class HandleError extends React.Component {
       column: 0,
       error: err.message,
     });
+    if (this.props.handleError) {
+      this.props.handleError(err);
+    }
   }
 
   render() {
